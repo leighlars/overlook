1c7e87b60b7bd567befc1d562a61d6042d69fb42
Leigh Larson <leighlarson4@gmail.com>
2020-09-02 20:38:28 -0600

    Modify tests on App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -8,12 +8,17 @@ describe('App', () => {
     render(<MemoryRouter><App/></MemoryRouter>)
   })
 
-  it('should render a landing page', () => {
+  it('should render a header always', () => {
     const logo = screen.getByRole('heading', {name: 'The Overlook'})
     const navLinks = screen.getAllByRole('link')
-    const welcomeImages = screen.getAllByRole('img')
     expect(logo).toBeInTheDocument()
     expect(navLinks).toHaveLength(4)
+  })
+
+  it('should render a landing page', () => {
+    const welcomeText = screen.getByRole('heading', {name: "Home"})
+    const welcomeImages = screen.getAllByRole('img')
+    expect(welcomeText).toBeInTheDocument()
     expect(welcomeImages).toHaveLength(6)
   })
 
