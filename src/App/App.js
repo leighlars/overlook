c5bc73ffe7a199e8521736749436e95a9c0d34db
Leigh Larson <leighlarson4@gmail.com>
2020-09-02 19:51:40 -0600

    Add styling to images in Landing Page

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import Header from "../Header/Header"
 import Main from '../Main/Main'
-
+// 
 class App extends Component {
   constructor() {
     super()
--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,30 +1,37 @@
 import React from "react";
 import "./LandingPage.scss";
-import HotelImage from "../HotelImage/HotelImage";
+import './HotelImage.scss'
+import bedroom from  "../images/bedroom.JPG";
+import hiddenBed from  "../images/hidden-bed.JPG";
+import lobby from  "../images/lobby.JPG";
+import restaurant from  "../images/restaurant.JPG";
+import stormyRoom from '../images/stormy-room.JPG'
+import suiteOffice from '../images/suite-office.JPG'
 
 const LandingPage = () => {
-  const images = [
-   { image: "../images/bedroom.jpg" },
-   { image: "../images/hidden-bed.jpg" },
-   { image: "../images/lobby.jpg" },
-   { image: "../images/restaurant.jpg" },
-   { image: "../images/stormy-room.jpg" },
-   { image: "../images/suite-office.jpg" },
-  ];
-  let hotelImages = images.map(image => {
-    return <HotelImage image={image} />
-  })
-
+  const hotelImages = () => {
+    return (
+     <div className='hotel-images'>
+       <img src={bedroom} className='hotel-image' alt='hotel with mountain'/>
+       <img src={hiddenBed} className='hotel-image' alt='hotel with mountain'/>
+       <img src={lobby} className='hotel-image' alt='hotel with mountain'/>
+       <img src={restaurant} className='hotel-image' alt='hotel with mountain'/>
+       <img src={stormyRoom} className='hotel-image' alt='hotel with mountain'/>
+       <img src={suiteOffice} className='hotel-image' alt='hotel with mountain'/>
+     </div> 
+    )
+  } 
+  
   return (
    <section className="landing-section">
-    <h3 className='landing-greeting'>Welcome Home</h3>
+    <h3 className="landing-greeting">Welcome Home</h3>
     <p className="welcome-text">
      Relax in the sauna. Hike numerous nearby trails. <br />
      Enjoy the views at sunset or sunrise. <br />
      At The Overlook, you're not just here, you're <br />
      <b>Home</b>
     </p>
-    <div className='hotel-images'>{hotelImages}</div>
+    {hotelImages()}
    </section>
   );
 
