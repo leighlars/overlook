ffabb5375aaf5697923843a2317f29f0459c3e25
Leigh Larson <leighlarson4@gmail.com>
2020-09-02 18:15:42 -0600

    Add Landing Section and styling

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,11 +4,11 @@ import "./LandingPage.scss";
 const LandingPage = () => {
   return (
    <section className="landing-section">
-    <h3>Welcome Home</h3>
+    <h3 className='landing-greeting'>Welcome Home</h3>
     <p className="welcome-text">
      Relax in the sauna. Hike numerous nearby trails. <br />
      Enjoy the views at sunset or sunrise. <br />
-     Here at The Overlook, you're not just here, you're <br />
+     At The Overlook, you're not just here, you're <br />
      <b>Home</b>
     </p>
    </section>
