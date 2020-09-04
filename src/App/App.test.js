15a68b95caf4eeafdb922727e20e9f2ed0ee4e02
Leigh Larson <leighlarson4@gmail.com>
2020-09-04 00:59:17 -0600

    Add passing fireEvent test in App for rendering login form from login navlink

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 import { MemoryRouter } from 'react-router-dom'
 
 describe('App', () => {
+  // the reason I did not use a beforeEach is because we will need to test the App in different views (logged in/out, manager, guest)
+
   it('should render a header always', () => {
     const {getByRole, getAllByRole} = render(<MemoryRouter><App/></MemoryRouter>)
     const logo = screen.getByRole('heading', {name: 'The Overlook'})
@@ -19,8 +21,19 @@ describe('App', () => {
    const welcomeImages = screen.getAllByRole("img");
    expect(welcomeText).toBeInTheDocument();
    expect(welcomeImages).toHaveLength(9);
-   // should make this more specific and not use getAllBy to check header
+   // should this be more specific and not use getAllBy to check header links?? 9 imgs was a lot to test for
   })
+
+  it("should render login form when Login link is clicked", () => {
+   const { getByRole } = render(<MemoryRouter> <App /></MemoryRouter>)
+   const loginLink = screen.getByRole("link", { name: "Login" })
+  //  check login link in header
+   fireEvent.click(loginLink);
+   // go to log in login page
+   // render login form
+   expect(screen.getByPlaceholderText("Username")).toBeInTheDocument()
+   const loginFormBtn = screen.getByRole("button", { name: "Login" })
+  });
 
 
 })
