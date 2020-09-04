ac4df51a4f09d402d6cb4608ec88900c076f9eb4
Leigh Larson <leighlarson4@gmail.com>
2020-09-04 00:25:53 -0600

    Add notes in App test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -4,22 +4,22 @@ import App from './App'
 import { MemoryRouter } from 'react-router-dom'
 
 describe('App', () => {
-  beforeEach(() => {
-    render(<MemoryRouter><App/></MemoryRouter>)
-  })
-
   it('should render a header always', () => {
+    const {getByRole, getAllByRole} = render(<MemoryRouter><App/></MemoryRouter>)
     const logo = screen.getByRole('heading', {name: 'The Overlook'})
     const navLinks = screen.getAllByRole('link')
     expect(logo).toBeInTheDocument()
     expect(navLinks).toHaveLength(4)
+    // should make this more specific and not use getAllBy to check header
   })
 
-  it('should render a landing page', () => {
-    const welcomeText = screen.getByRole('heading', {name: "Home"})
-    const welcomeImages = screen.getAllByRole('img')
-    expect(welcomeText).toBeInTheDocument()
-    expect(welcomeImages).toHaveLength(6)
+  it('should render a landing page if not logged in', () => {
+   const { getByRole, getAllByRole } = render(<MemoryRouter> <App /></MemoryRouter>);
+   const welcomeText = screen.getByRole("heading", { name: "Home" });
+   const welcomeImages = screen.getAllByRole("img");
+   expect(welcomeText).toBeInTheDocument();
+   expect(welcomeImages).toHaveLength(9);
+   // should make this more specific and not use getAllBy to check header
   })
 
 
