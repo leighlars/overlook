0977ce4c0aadf5a2a0abacb94bd50ad4b1e059aa
Leigh Larson <leighlarson4@gmail.com>
2020-09-04 00:22:02 -0600

    Add notes in Landing Page test

--- src/LandingPage/LandingPage.test.js
+++ src/LandingPage/LandingPage.test.js
@@ -12,6 +12,7 @@ describe('LandingPage', () => {
  })
 
   it('should render a greeting text and array of images', () => {
+    // how do we test for the larger welcoming text on landing page?
     const greeting = screen.getByRole('heading', {name: 'Home'})
     const images = screen.getAllByRole('img')
     expect(greeting).toBeInTheDocument()
