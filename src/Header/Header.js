81a4309e3de0ef5717cff08dad89df63d673a44d
Leigh Larson <leighlarson4@gmail.com>
2020-09-04 01:04:33 -0600

    Add conditional to render welcome greeting to user in header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,19 +7,22 @@ const Header = ({isLoggedIn, user}) => {
   return (
    <header>
     <h1 className="header-logo">The Overlook</h1>
-    <div className="nav-btns">
-     <NavLink to="/" className="nav-btn home">
-      Home
-     </NavLink>
-     <NavLink to="/login" className={isLoggedIn ? "hidden" : "nav-btn login"}>
-      Login
-     </NavLink>
-     <NavLink to="/" className={isLoggedIn ? "nav-btn logout" : 'hidden'}>
-      Logout
-     </NavLink>
-     <NavLink to="/user-home" className={(isLoggedIn && user !== 'manager') ? "nav-btn user profile" : 'hidden'}>
-      My Profile
-     </NavLink>
+    <div className='right-header'>
+      {user.name && <h3 className='welcome-greeting'>Welcome, {user.name}</h3>}
+      <div className="nav-btns">
+      <NavLink to="/" className="nav-btn home">
+        Home
+      </NavLink>
+      <NavLink to="/login" className={isLoggedIn ? "hidden" : "nav-btn login"}>
+        Login
+      </NavLink>
+      <NavLink to="/" className={isLoggedIn ? "nav-btn logout" : 'hidden'}>
+        Logout
+      </NavLink>
+      <NavLink to="/user-home" className={(isLoggedIn && user !== 'manager') ? "nav-btn user profile" : 'hidden'}>
+        My Profile
+      </NavLink>
+      </div>
     </div>
    </header>
   );
