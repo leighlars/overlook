e1e39571539dbfbcf5cfca8064a9e080dc8dead8
Amy Karnaze <akarnaze@gmail.com>
2020-09-08 19:52:20 -0600

    Prepare postNewBooking and deleteNewBooking for export and json only

--- src/API/API.js
+++ src/API/API.js
@@ -17,30 +17,36 @@ const rootURL = "https://fe-apps.herokuapp.com/api/v1/overlook/1904/";
      .then(response => response.json())
  }
 
- postNewBooking(newBooking) {
-  fetch(`${rootURL}bookings/bookings`, {
-   method: "POST",
-   headers: { "Content-Type": "application/json" },
-   body: JSON.stringify(newBooking),
-  })
-   .then((response) => response.json())
-   .then((data) => {
-    console.log("Success:", data);
-   })
-   .catch((error) => {
-    console.log("Failed:", error);
-   });
+ export const postNewBooking = (newBooking) => {
+   fetch(`${rootURL}bookings/bookings`, {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json"
+       },
+       body: JSON.stringify(newBooking),
+     })
+     .then((response) => response.json())
+   //below code will go in appropriate component
+   //  .then((data) => {
+   //   console.log("Success:", data);
+   //  })
+   //  .catch((error) => {
+   //   console.log("Failed:", error);
+   //  });
  }
 
- deleteBooking(deletedBooking) {
-  fetch(`${rootURL}bookings/bookings`, {
-   method: "DELETE",
-   headers: { "Content-Type": "application/json" },
-   body: JSON.stringify(deletedBooking),
-  })
-   .then((res) => res.json())
-   .then((data) => {
-    console.log("Success", data);
-   })
-   .catch((err) => console.log(err));
+ export const deleteBooking = (deletedBooking) => {
+   fetch(`${rootURL}bookings/bookings`, {
+       method: "DELETE",
+       headers: {
+         "Content-Type": "application/json"
+       },
+       body: JSON.stringify(deletedBooking),
+     })
+     .then((res) => res.json())
+   //below code will go in appropriate component
+   //  .then((data) => {
+   //   console.log("Success", data);
+   //  })
+   //  .catch((err) => console.log(err));
  }
